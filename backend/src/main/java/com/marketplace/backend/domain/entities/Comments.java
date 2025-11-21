11533d91662b63f636cfcef23882e31d04a2d6b2
Rodrii14 <00087022@uca.edu.sv>
2025-11-21 15:57:55 -0600

    Add: reply to comments

--- backend/src/main/java/com/marketplace/backend/domain/entities/Comments.java
+++ backend/src/main/java/com/marketplace/backend/domain/entities/Comments.java
@@ -1,10 +1,13 @@
 package com.marketplace.backend.domain.entities;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -22,6 +25,9 @@ public class Comments {
     @Column(name = "comment")
     private String comment;
 
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comments> responses = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "app_user")
     private User user;
@@ -29,4 +35,13 @@ public class Comments {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product")
     private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comments parent;
+
+    public void addResponse(Comments response){
+        responses.add(response);
+        response.setParent(this);
+    }
 }
